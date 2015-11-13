4891b12da31a6f835d72885137f1c65a06840801
Kerem Gunes <qeremy@gmail.com>
2015-11-14 00:50:38 +0200

    Test mkdir issue.

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 var execSync = require("./index.js");
 
 // direct
-// var output = execSync("ls -la");
-// console.log(output)
+var output = execSync("ls -la");
+console.log(output);
 
 // with callback
 // execSync("ls -la", {}, console.log)
-execSync("ls -la", {}, function(output){
-    console.log(output)
-});
+// execSync("ls -la", {}, function(output){
+//     console.log(output)
+// });
