24dcc5ffc022ec63055fd35ffe1e96d6a3f2b1e0
Kerem Gunes <qeremy@gmail.com>
2015-11-13 23:51:37 +0200

    Add LICENSE.

--- src/exec-sync.js
+++ src/exec-sync.js
@@ -1,4 +1,12 @@
 /**
+ * Copyright (c) 2015 Kerem Güneş
+ *    <http://qeremy.com>
+ *
+ * The MIT License
+ *    <http://opensource.org/licenses/MIT>
+ */
+
+/**
  * Module objects.
  * @private
  */
